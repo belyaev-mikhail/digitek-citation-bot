c2722bfcc9eff7cad38adf6c2a69a5c4001048d7
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2021-06-17 13:06:10 +0300

    Do not ban empty messages

--- Code.ts
+++ Code.ts
@@ -631,11 +631,13 @@ type Paranoid<Obj> =
 
 function checkBan(message: Paranoid<Message>): boolean {
     const banlist = getBanList()
-    return message?.chat?.id?.toString() in banlist
-        || message?.from?.id.toString() in banlist
-        || message?.from?.first_name in banlist
-        || message?.from?.username in banlist
-        || ("@" + message?.from?.username) in banlist;
+    try {
+        return message?.chat?.id?.toString() in banlist
+            || message?.from?.id.toString() in banlist
+            || message?.from?.first_name in banlist
+            || message?.from?.username in banlist
+            || ("@" + message?.from?.username) in banlist;
+    } catch (e) { return false }
 }
 
 function handleMessage(message: Message) {
